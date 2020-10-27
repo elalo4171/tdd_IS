417ad9882384bf750ce259748f28603ec185091d
Eduardo Garcia <elalo417@gmail.com>
2020-10-26 18:55:49 -0600

    init kata

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hola mundo");
+        kata_bowling kata= new kata_bowling();
+        kata.init_kata();
     }
 }
---
+++ kata_bowling.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class kata_bowling {
+
+    public void init_kata(){
+        System.out.println("Init kata");
+    }
+}
